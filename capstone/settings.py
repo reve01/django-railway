f171f2d226a73d30182fbc10fa0f5e99230aabbf
gonza <gonza@gmail.cl>
2024-12-08 13:23:48 -0300

    junction

--- capstone/settings.py
+++ capstone/settings.py
@@ -101,10 +101,7 @@ WSGI_APPLICATION = 'capstone.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
